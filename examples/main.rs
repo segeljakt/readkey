a67e62443df42479d4db9b17846a5dcef32f3742
Klas Segeljakt <klasseg@kth.se>
2025-03-04 10:37:03 +0100

    Update example to handle all arrow keys

--- examples/main.rs
+++ examples/main.rs
@@ -1,11 +1,20 @@
-
-
 extern crate readkey;
 
 use readkey::Keycode;
 
 fn main() {
-  loop {
-    println!("State of Up key: {}", Keycode::Up.is_pressed());
-  }
+    loop {
+        if Keycode::Up.is_pressed() {
+            println!("Up is pressed");
+        }
+        if Keycode::Right.is_pressed() {
+            println!("Right is pressed");
+        }
+        if Keycode::Left.is_pressed() {
+            println!("Left is pressed");
+        }
+        if Keycode::Down.is_pressed() {
+            println!("Down is pressed");
+        }
+    }
 }
